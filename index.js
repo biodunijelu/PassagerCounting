52022d9bf282a170ae95ea77a307582d90f4e733
BiodunIjelu <biodunijelu@gmail.com>
2022-05-23 12:33:16 +0100

    Walkway

---
+++ index.js
@@ -0,0 +1,31 @@
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+// camelCase
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el") // pass in arguments
+let count = 0
+
+function increment() {
+
+    count = count + 1
+    countEl.textContent = count
+   
+}
+
+// 1. Create a function, save(), which logs out the count when it's called
+// 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+// 3. Render the variable in the saveEl using innerText
+// NB: Make sure to not delete the existing content of the paragraph
+function save() {
+
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+
+
